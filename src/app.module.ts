dff2e3947359a18e51d37f22021a115a91e359e3
nagipogu <giribabu453@gmail.com>
2022-01-09 21:32:49 +0530

    nest js api

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb://localhost/products',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
